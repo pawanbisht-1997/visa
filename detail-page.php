ba5ca374a36830118199704754e3a2be9fa81fcc
pawanbisht-1997 <106995482+pawanbisht-1997@users.noreply.github.com>
2023-07-05 19:08:26 +0530

    Add files via upload

---
+++ detail-page.php
@@ -0,0 +1,131 @@
+<?php include 'inc/head.php';?>
+<section class="detail-box">
+    <div class="container">
+       <div class="row">
+        <div class="col-md-8">
+            <div class="left">
+                <div class="box-shadow mb-4">
+                    <div class="top-heading">
+                        <div class=" mb-3">
+
+                            <h3>ANTIGUA AND BARBUDA VISA</h3>
+                        </div>
+                    </div>
+                    <div class="left-img">
+                        <img src="./img/Armenia-travel-AP73256745.jpg" alt="" class="img-fluid mb-4">
+                    </div>
+                    <div class="box">
+
+                        <p>The land of mystic, ancient cultures and amazing architecture is a spot on tourist attraction
+                            for travelers from across the world. Armenia has its own set of different cultures and
+                            traditions and that’s what makes Armenia a gem for tourism. When it comes to Armenia, the
+                            country has a lot to offer and getting there has never been easier. Apply for Armenian visa
+                            for Indians on our website and visit this beautiful country. We offer the most accurate
+                            details for Armenian e-visa which you can find below. </p>
+                    </div>
+                </div>
+                <div class="box-shadow mb-4">
+                    <h5 class="mb-2">ELIGIBILITY CRITERIA FOR ARMENIA VISA</h5>
+                    <ul>
+                        <li>Indian National holding a valid Indian passport can apply for Armenia Visa online.</li>
+                        <li> Person who wishes to travel to Armenia for the purpose of Tourism can apply visa through
+                            Visa Lounge. Person who seeks employment in Armenia is not eligible to apply through Visa
+                            Lounge.</li>
+                        <li>We suggest applicants to carry Travel Insurance while travelling to Armenia. Visa Lounge
+                            also provides Travel Insurance to passengers at an additional cost.
+
+                        </li>
+                        <li>Clear Colored Front and Last full page of Valid Indian passport copies with minimum validity
+                            of six months.
+
+                        </li>
+
+                    </ul>
+                </div>
+                <div class="box-shadow mb-4">
+                    <h5 class="mb-2">DOCUMENTS REQUIRED (Scanned copies):</h5>
+                    <ul>
+                        <li> Clear Colored Front and Last full page of Valid Indian passport copies with minimum
+                            validity of six months.
+
+                        </li>
+                        <li> Colour Photo - Specification 35 mm X 45 mm, 65% to 70% face cover with both the ears
+                            visible & white back ground.
+
+                        </li>
+                        <li> Hotel Confirmation details.
+
+                        </li>
+                        <li> Ticket copy.
+
+                        </li>
+                        <li> Applicant's Mobile number.
+
+                        </li>
+                        <li>Travel insurance copy.
+
+                        </li>
+                        <li> Bank statement for last 3 months.
+
+                        </li>
+                        <li> If onward journey to third country then required evidence that you are assured entry into
+                            the onward country that you are travelling to, such as: residence permit· ‘green card’·
+                            valid visa·
+
+                        </li>
+
+
+                    </ul>
+                </div>
+                <div class="box-shadow">
+                    <h5 class="mb-2">ARMENIA VISA FEES</h5>
+                    <table class="table table-bordered">
+
+                        <thead>
+                            <tr>
+                                <th>Visa Types</th>
+                                <th>Normal Price (INR)*</th>
+                                <th>Express Price (INR)*</th>
+                                <th>Entry</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            <tr>
+                                <td>21 Days Tourist Visa
+
+                                </td>
+                                <td>1200 </td>
+                                <td>NA </td>
+                                <td>Single</td>
+                            </tr>
+                            <tr>
+                                <td>120 Days Tourist Visa
+
+                                </td>
+                                <td>3300</td>
+                                <td>NA</td>
+                                <td>Single</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+           <div class="inner_form">
+            <div class="contact">
+                <div class="right">
+                    <h4>Get Your Visa Now</h4>
+                    <p class="text-white">Please fill out the form below</p>
+
+                    <?php include 'inc/form.php';?>
+                </div>
+               </div>
+           </div>
+        </div>
+       </div>
+    </div>
+</section>
+
+<?php include 'inc/footer.php';?>
